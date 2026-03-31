6565e16594a7897fa69582b6446185013f8d1d63
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-31 09:02:43 +0200

    we restore the list Group Item layout

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -13,11 +13,17 @@ function ListGroup() {
       <h1>List Group</h1>
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
         ))}
       </ul>
     </>
   );
 }
+
+// TODO Restore the List Group build
+// TODO add an Condition for when the list is empty
+// TODO logging cklicking events
 
 export default ListGroup;
